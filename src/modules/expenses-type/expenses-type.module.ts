9c750556149fd17d753f3dd97d08212913d0a916
Omar Guanoluisa <omar.guanoluisa25@gmail.com>
2024-06-25 22:09:15 -0500

    configuration to connect entity to table in database

--- src/modules/expenses-type/expenses-type.module.ts
+++ src/modules/expenses-type/expenses-type.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ExpensesTypeService } from './services/expenses-type.service';
 import { ExpensesTypeController } from './controllers/expenses-type.controller';
+import { ExpensesType } from './entities/expenses-type.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ExpensesType])],
   controllers: [ExpensesTypeController],
   providers: [ExpensesTypeService],
 })
--- src/modules/sales/sales.module.ts
+++ src/modules/sales/sales.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SalesService } from './services/sales.service';
 import { SalesController } from './controllers/sales.controller';
+import { Sale } from './entities/sale.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Sale])],
   controllers: [SalesController],
   providers: [SalesService],
 })
